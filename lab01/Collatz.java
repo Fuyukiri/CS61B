1c5cd37376fa957177d6aa214256958237d40ad7
AntiWall <ns34gp@hotmail.com>
2022-11-12 00:26:41 -0500

    fix buggy version

--- lab01/Collatz.java
+++ lab01/Collatz.java
@@ -3,22 +3,10 @@
  */
 public class Collatz {
 
-<<<<<<< HEAD
     /** Returns the nextNumber in a Collatz sequence. */
     public static int nextNumber(int n) {
         // TODO: Fill in this method.
         return 1;
-=======
-    /** Buggy implementation of nextNumber! */
-    public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
-        }
->>>>>>> ca91837418f0c4f7449ec7916d28103aed61f5e3
     }
 
     public static void main(String[] args) {
