12738f753ed4ecf797cedaedd48c7eaa59f4b240
Fuyukiri <11898971+Fuyukiri@users.noreply.github.com>
2022-11-16 21:23:08 -0800

    proj1 Drum Harp

--- proj1/gh2/Harp.java
+++ proj1/gh2/Harp.java
@@ -1,5 +1,16 @@
 package gh2;
 
-public class Harp {
+import deque.LinkedListDeque;
 
+public class Harp extends GuitarString{
+    private static final double DECAY = .997;
+    public Harp(double frequency) {
+        super(frequency * 2);
+    }
+    @Override
+    public void tic() {
+        double frontSample = buffer.removeFirst();
+        double newDouble = (frontSample + buffer.get(0)) / 2 * DECAY;
+        buffer.addLast(-newDouble);
+    }
 }
