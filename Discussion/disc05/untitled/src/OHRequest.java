440f740ef238cf75f89dc8bebe0dfcc599591639
Fuyukiri <11898971+Fuyukiri@users.noreply.github.com>
2022-11-27 13:19:52 -0800

    hw02

--- Discussion/disc05/untitled/src/OHRequest.java
+++ Discussion/disc05/untitled/src/OHRequest.java
@@ -1,2 +1,73 @@
-package PACKAGE_NAME;public class OHRequest {
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class OHRequest {
+    public String description;
+    public String name;
+    public OHRequest next;
+
+    public OHRequest(String description, String name, OHRequest next) {
+        this.description = description;
+        this.name = name;
+        this.next = next;
+    }
+}
+
+class OHIterator implements Iterator<OHRequest> {
+    OHRequest curr;
+
+    public OHIterator(OHRequest queue) {
+        curr = queue;
+    }
+
+    public boolean isGood(String description) {
+        return true;
+    }
+
+
+    @Override
+    public boolean hasNext() {
+        while (curr != null && !isGood(curr.description)) {
+            curr = curr.next;
+        }
+        return curr != null;
+    }
+
+    @Override
+    public OHRequest next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        OHRequest currRequest = curr;
+        curr = curr.next;
+        return currRequest;
+    }
+}
+
+class OHQueue implements Iterable<OHRequest> {
+    OHRequest queue;
+
+    public OHQueue(OHRequest queue) {
+        this.queue = queue;
+    }
+
+    @Override
+    public Iterator<OHRequest> iterator() {
+        return new OHIterator(queue);
+    }
+}
+
+class TYIterator extends OHIterator {
+    public TYIterator(OHRequest queue) {
+        super(queue);
+    }
+
+    @Override
+    public OHRequest next() {
+        OHRequest result = super.next();
+        if (result.description.contains("thank u")) {
+            result = super.next();
+        }
+        return result;
+    }
 }
--- hw2/test/PercolationStatsTest.java
+++ hw2/test/PercolationStatsTest.java
@@ -3,13 +3,28 @@ package test;
 import hw2.PercolationFactory;
 import hw2.PercolationStats;
 import org.junit.Test;
+import edu.princeton.cs.algs4.Stopwatch;
 
 public class PercolationStatsTest {
+
+    private static int N = 100;
+    private static int T = 300;
     @Test
     public void testPercolationStats() {
-        PercolationStats percolationStats = new PercolationStats(10, 30, new PercolationFactory());
-        System.out.println(percolationStats.mean());
-        System.out.println(percolationStats.stddev());
-        System.out.println(percolationStats.confidenceHigh());
+        timeCheck(N, T);
+        timeCheck(N * 2, T);
+        timeCheck(N, T * 2);
+        timeCheck(N * 2, T * 2);
+    }
+
+    private void timeCheck(int N, int T) {
+        Stopwatch stopwatch = new Stopwatch();
+        PercolationStats percolationStats = new PercolationStats(N, T, new PercolationFactory());
+        percolationStats.stddev();
+        percolationStats.confidenceHigh();
+        percolationStats.confidenceLow();
+        double time = stopwatch.elapsedTime();
+        System.out.println("N: " + N + " T: " + T);
+        System.out.println("Time: " + time);
     }
 }
